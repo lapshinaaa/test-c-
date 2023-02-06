a4d465e628d054bd0784edf7ad39ab75e06ac31d
Anastasiia Lapshina <91057358+lapshinaaa@users.noreply.github.com>
2023-02-06 09:58:08 +0300

    add task 3 | sum of diagonal elements

---
+++ sum of diagonal.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+
+int sumMainDiagonal(int **arr, int rows, int columns)
+{
+    int sum = 0;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < columns; ++j) {
+            if (i == j) sum += arr[i][j];
+        }
+    }
+
+    return sum;
+
+}
+
+void fillMatrix (int **array, int rows, int columns)
+{
+    //Taking input in the array
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i][j] = rand() % 100;
+        }
+    }
+}
+
+
+void printMatrix (int **array, int rows, int columns)
+{
+    //printing out the given array
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            std::cout<<array[i][j]<<" ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+int main()
+{
+    int **array;
+    int rows = 0;
+    int columns = 0;
+    int sum = 0;
+
+    std::cin>>rows;
+    std::cin>>columns;
+
+    //Dynamically allocating rows space in heap
+    array = new int*[rows];
+    //Dynamically allocating column space in heap
+    for(int i = 0; i < rows; i++){
+        array[i] = new int[columns];
+    }
+
+    fillMatrix(array, rows, columns);
+    std::cout<<"\n";
+    printMatrix(array, rows, columns);
+
+    sum = sumMainDiagonal(array, rows, columns);
+    std::cout<<"The sum of the elements of the main diagonal us equal to: "<< sum<<std::endl;
+
+    return 0;
+}
